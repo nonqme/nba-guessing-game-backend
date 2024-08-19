315576f1434e3bd28312f1201b87dd35b1cf09b0
nonqme <w.missary@gmail.com>
2024-08-19 19:47:20 +0200

    chore: Starting v2

--- src/commons/Fetcher.ts
+++
@@ -1,7 +0,0 @@
-import { IFetcher } from '../interfaces';
-
-export class Fetcher implements IFetcher {
-  async fetch(url: URL, options: RequestInit): Promise<Response> {
-    return fetch(url, options);
-  }
-}
